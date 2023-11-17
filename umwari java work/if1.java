2de30e670c4e344343192ef9a4399bb095ba789a
Umwari2 <151011807+Umwari2@users.noreply.github.com>
2023-11-17 10:00:56 +0200

    Add files via upload

---
+++ umwari java work/if1.java
@@ -0,0 +1,9 @@
+public class if1 {    
+public static void main(String[] args) {    
+int x = 5;    
+int y = 12;    
+if(x+y < 20) {    
+System.out.println("x + y is less than 20");    
+}    
+}      
+}     
---
+++ umwari java work/if2.java
@@ -0,0 +1,9 @@
+public class if2 {    
+public static void main(String[] args) {    
+int number = 10;    
+   
+if (number > 0) {    
+System.out.println("the number is positive.");    
+}    
+}      
+}     
